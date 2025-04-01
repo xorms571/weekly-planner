450d3e7b8b9dc84aa1b9305b6b1be54d00862940
xorms571 <xorms6865@naver.com>
2025-04-02 02:56:03 +0900

    몽고db 로직수정 for vercel 배포

--- src/app/api/lib/mongodb.ts
+++ src/app/api/lib/mongodb.ts
@@ -2,25 +2,24 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 
 const uri = process.env.MONGO_DB_URI;
 if (!uri) {
-  throw new Error("MONGODB_URI is not defined in .env.local");
+  throw new Error("MONGO_DB_URI is not defined in .env.local");
 }
 
 let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
+const clientPromise: Promise<MongoClient> =
+  global._mongoClientPromise ||
+  (async () => {
+    client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
 
-if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  });
-
-  global._mongoClientPromise = client.connect();
-}
-
-clientPromise = global._mongoClientPromise;
+    global._mongoClientPromise = client.connect();
+    return global._mongoClientPromise;
+  })();
 
 export async function connectToDatabase() {
   const client = await clientPromise;
