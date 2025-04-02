6408156479dff9dd722c67868b592292153a3875
xorms571 <xorms6865@naver.com>
2025-04-02 18:06:24 +0900

    options 변경

--- src/app/api/lib/mongodb.ts
+++ src/app/api/lib/mongodb.ts
@@ -4,9 +4,8 @@ const uri = process.env.MONGO_DB_URI;
 if (!uri) throw new Error("MONGO_DB_URI is not defined in .env.local");
 
 const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   tls: true,
+  tlsAllowInvalidCertificates: true,
 };
 
 const globalWithMongo = global as unknown as NodeJS.Global;
