5e49949ef64e4e5e20820eb2a561a370ee48e367
xorms571 <xorms6865@naver.com>
2025-04-02 18:58:07 +0900

    몽고db수정

--- src/app/api/lib/mongodb.ts
+++ src/app/api/lib/mongodb.ts
@@ -3,15 +3,10 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGO_DB_URI;
 if (!uri) throw new Error("MONGO_DB_URI is not defined in .env.local");
 
-const options = {
-  tls: true,
-  tlsAllowInvalidCertificates: true,
-};
-
 const globalWithMongo = global as unknown as NodeJS.Global;
 
 if (!globalWithMongo._mongoClientPromise) {
-  const client = new MongoClient(uri, options);
+  const client = new MongoClient(uri);
   globalWithMongo._mongoClientPromise = client.connect();
 }
 
